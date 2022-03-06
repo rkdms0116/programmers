fff8f93a1a0c3639c0e1f30885c85a4cdf729cf8
rkdms0116 <rkdms0116@gmail.com>
2022-03-06 22:23:20 +0900

    UPDATE: best_album

--- 해시/베스트_앨범.py
+++ 해시/베스트_앨범.py
@@ -1,44 +1,36 @@
 def solution(genres, plays):
     answer = []
-    """
-    # 장르/장르의 재생 횟수
+    # play_gen = {장르 : 장르 누적 재생 횟수}
     play_gen = {}
-    # 장르/노래 index/재생 횟수
-    play_song = {}
     for gen in range(len(genres)):
-        if gen in play_gen:
+        if genres[gen] in play_gen:
             play_gen[genres[gen]] += plays[gen]
         else:
             play_gen[genres[gen]] = plays[gen]
-    # 가장 많이 재생된 장르대로 순서
-    sort_play = sorted(play_gen.items(), key = lambda item: item[1], reverse=True)
-    print(sort_play)
-    """
-    # genres와 plays dictionary 형태로 묶어서 최대 play된 장르 순서대로 정렬
-    genresNplays = sorted(dict(zip(genres, plays)).items(), key = lambda item: item[1], reverse=True)
-    """
-    play = {}
-    while plays:
-        for n in range(len(genres)):
-            play_genre = {}
-            for g in range(len(genresNplays)):
-                if genres[n] == genresNplays[g]:
-                    play_genre[n] = plays[n]
-                    plays.pop(plays[n])
-            play_genre_sort = sorted(play_genre.items(), key = lambda item: item[1], reverse=True)
-            for sor in range(len(play_genre_sort)):
-                answer.append(play_genre_sort)
-    print(play)
-    """
+    # genresNplays : play_gen 내림차순 정렬
+    genresNplays = sorted(play_gen.items(), key = lambda item: item[1], reverse=True)
+
+    # playNindex : {index : [장르, 곡 재생 횟수]}
     playNindex = {}
     for num in range(len(plays)):
         if genres[num] not in playNindex:
             playNindex[num] = [genres[num], plays[num]]
         else:
             playNindex[num] = [genres[num], plays[num]]
-    print(playNindex)
-    for gen in genresNplays:
-        for idx in range(len(playNindex)):
-            if playNindex[idx][1] == gen:
-                pass
+
+    # 최대 장르를 for문으로 순회하면서
+    for n in range(len(genresNplays)):
+        play_list = {}
+        # playNindex의 장르가 만약에 일치한다면
+        for i in range(len(playNindex)):  
+            if genresNplays[n][0] == playNindex[i][0]:
+                # play_list = {index : 재생 횟수}
+                play_list[i] = playNindex[i][1]
+        # play 수에 따라 정렬
+        temp_list = sorted(play_list.items(), key = lambda item: item[1], reverse=True)
+        # 첫번째 인덱스 저장
+        answer.append(temp_list[0][0])
+        # temp_list가 2개 이상이라면 1개만 더 저장하고 종료
+        if len(temp_list) > 1 :
+            answer.append(temp_list[1][0])
     return answer
