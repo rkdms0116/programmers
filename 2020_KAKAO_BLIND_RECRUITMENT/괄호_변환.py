bc7d6e8913dbb0375f8dcb6eb4c1c99dcd55e7e6
rkdms0116 <rkdms0116@gmail.com>
2022-05-29 11:27:52 +0900

    괄호변환_ing

--- 2020_KAKAO_BLIND_RECRUITMENT/괄호_변환.py
+++ 2020_KAKAO_BLIND_RECRUITMENT/괄호_변환.py
@@ -1,53 +1,29 @@
-def solution(p):
-    answer = ''
-    if not(p):
-        return p
-
-    if check(p):
-        return p
-
-    [u, v] = sep(p)
-
-    if check(u):
-        return u + solution(v)
+def solution(check_words):
+    if check_words:
+        open_cnt = 0
+        for i in range(1, len(check_words)):
+            word = check_words[:i]
+            if word.count("(") == word.count(")"):
+                u = word
+                v = check_words[i:]
+                print(u, v)
+                break
+    # 빈 문자열일 경우
     else:
-        answer += '('
-        for i in range(len(u)-2,0,-1):
-            answer += u[i]
-        answer += ')'
-        
-    return answer
-        
-def check(p):
-    x = y = 0
-    for i in range(len(p)):
-        if i == 0:
-            if p[i] == '(':
-                x += 1
-            else:
-                return False
+        return ""
+
+solution("()))((()")
+
+def check(check_words):
+    cnt = 0
+    for word in check_words:
+        if cnt < 0 :
+            change(check_words)
+
+        if word == "(":
+            cnt += 1
         else:
-            if y == x:
-                return False
-            if p[i] == '(':
-                x += 1
-            else:
-                y += 1
-            if y == x and x+y == len(p):
-                return True
-    else:
-        return True
-    
-def sep(p):
-    x = y = 0
-    for i in range(len(p)):
-        if p[i] == '(':
-            x += 1
-        else:
-            y += 1
-        if x==y:
-            u = p[:i+1]
-            v = p[i+1:]
-            return [u,v]
-print(check("())(()"))
-print(solution("()))((()"))
+            cnt -=1
+
+def change(check_words):
+    pass
