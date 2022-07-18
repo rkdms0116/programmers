e73174f82ffdf71ae7fd2a5bb294b6729ab8d343
rkdms0116 <rkdms0116@gmail.com>
2022-07-18 15:26:24 +0900

    튜플

--- 2019_카카오_개발자_겨울_인턴십/튜플.py
+++ 2019_카카오_개발자_겨울_인턴십/튜플.py
@@ -1,26 +1,11 @@
 def solution(s):
-    answer = ""
-    s = s[1:-1]
-    temp1 = s.replace('{', '[')
-    temp2 = temp1.replace('}', ']')
-    
-    i = 0
-    temp = ""
-    while i < len(temp2):
-
-        if temp2[i] == "]":
-            temp += temp2[i]
-            if len(temp) > len(answer):
-                answer = temp
-            temp = ""
-            i += 1
-
-        if i >= len(temp2):
-            break
-        temp += temp2[i]
-        i += 1
-                    
+    words = s[2:-2]
+    li = words.split('},{')
+    answer = []
+    li.sort(key=len)
+    for l in li:
+        num = l.split(',')
+        for n in num:
+            if int(n) not in answer:
+                answer.append(int(n))
     return answer
-
-s = "{{20,111},{111}}"
-print(solution(s))
