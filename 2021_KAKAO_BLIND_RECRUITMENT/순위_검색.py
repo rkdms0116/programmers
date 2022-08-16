1c2c4974eff39be9da2cbfe06b90ac806d204deb
rkdms0116 <rkdms0116@gmail.com>
2022-08-16 10:50:16 +0900

    삼각 달팽이 solve, 순위 검색 ing

--- 2021_KAKAO_BLIND_RECRUITMENT/순위_검색.py
+++ 2021_KAKAO_BLIND_RECRUITMENT/순위_검색.py
@@ -1,27 +1,29 @@
+from itertools import combinations
+
 def solution(info, query):
     answer = []
-    # query에 있는 조건 하나씩 뽑아서 list에 담기
-    conditions = [quer.split() for quer in query]
-    for condition in conditions:
-        # and 없애기
-        condition.pop(1)
-        condition.pop(2)
-        condition.pop(3)
-        # 조건에 맞는 인원 count
+    
+    for quer in query:
+        temp_con = quer.split()[:-1]
+        score = quer.split()[-1]
+        condition = ''
+        for t in temp_con:
+            if t != '-' and t != 'and':
+                condition += t
         cnt = 0
-        # 사람들의 정보를 탐색
+
         for i in info:
-            # 
-            person = list(i.split())
-            for p in range(len(person)):
-                if person[p] == "-":
-                    pass
-                elif person[p] != condition[p]:
-                    break
-            else:
-                cnt += 1
+            info_temp = list(i.split()[:4])
+            info_val = i.split()[-1]
+            info_key = []
+            if int(score) <= int(info_val):
+                for k in range(5):
+                    for combination in combinations(info_temp, k):
+                        info_key.append(''.join(combination))
+                if condition in info_key:
+                    cnt += 1
         answer.append(cnt)
-        
+                        
     return answer
 
 print(solution(["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"], ["cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]))
