5f590db3ab3e2cb57ef92e8a2c53fdddf811f8a9
rkdms0116 <rkdms0116@gmail.com>
2022-12-09 22:52:05 +0900

    연속 부분 수열 합의 개수 pass

--- 연습문제/가장_큰_정사각형_찾기.py
+++ 연습문제/가장_큰_정사각형_찾기.py
@@ -1,13 +1,16 @@
 def solution(board):
     answer = 0
-    if len(board) < 2 and 1 in board[0]:
-        return 1
     for i in range(len(board)):
         for j in range(len(board[0])):
-            if board[i][j]:
-                board[i][j] = min(board[i-1][j-1], board[i-1][j], board[i][j-1]) + 1
-                answer = max(answer, board[i][j])
+            if i==0 or j==0:
+                pass
+            elif board[i][j] and board[i][j-1] and board[i-1][j] and board[i-1][j-1]:
+                board[i][j] = board[i-1][j-1] +1
+            answer = max(answer, board[i][j])
+    print(board)
     return answer**2
-board = [[1]]
-solution(board)
-print(board)
+
+
+print(solution([[0,0,0],[0,0,0]]))
+print(solution([[1, 1, 1], [1, 0, 1], [1, 1, 1]]))
+print(solution([[0, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [0, 0, 1, 0]]))
--- 연습문제/연속_부분_수열_합의_개수.py
+++ 연습문제/연속_부분_수열_합의_개수.py
@@ -20,3 +20,18 @@ def solution(elements):
             cnt += 1
 
     return len(sum_set)
+
+# pass code
+def solution(elements):
+    # 합을 저장하는 set
+    sum_set = set()
+    
+    # 원형 수열
+    que = elements * 2
+    # 시작하는 index
+    for i in range(len(elements)):
+        # slice의 범위
+        for j in range(1, len(elements)+1):
+            sum_set.add(sum(que[i:i+j]))
+
+    return len(sum_set)
